12d77c26c13a71428a7313e32a2692caa417a71a
Youssef Shaheen <shaheeny616@gmail.com>
2024-08-28 21:32:43 +0300

    Other Promise Combinators race, allSettled and any

--- Main.js
+++ Main.js
@@ -1477,24 +1477,88 @@ const getJSON = function (url, errorMsg = 'country not found') {
 //   }
 // })();
 
-const get3Countries = async function (c1, c2, c3) {
-  try {
-    // const [data1] = await getJSON(`https://restcountries.com/v2/name/${c1}`);
-    // const [data2] = await getJSON(`https://restcountries.com/v2/name/${c2}`);
-    // const [data3] = await getJSON(`https://restcountries.com/v2/name/${c3}`);
+// const get3Countries = async function (c1, c2, c3) {
+//   try {
+//     // const [data1] = await getJSON(`https://restcountries.com/v2/name/${c1}`);
+//     // const [data2] = await getJSON(`https://restcountries.com/v2/name/${c2}`);
+//     // const [data3] = await getJSON(`https://restcountries.com/v2/name/${c3}`);
 
-    // console.log([data1.capital, data2.capital, data3.capital]);
+//     // console.log([data1.capital, data2.capital, data3.capital]);
 
-    const data = await Promise.all([
-      getJSON(`https://restcountries.com/v2/name/${c1}`),
-      getJSON(`https://restcountries.com/v2/name/${c2}`),
-      getJSON(`https://restcountries.com/v2/name/${c3}`),
-    ]);
+//     const data = await Promise.all([
+//       getJSON(`https://restcountries.com/v2/name/${c1}`),
+//       getJSON(`https://restcountries.com/v2/name/${c2}`),
+//       getJSON(`https://restcountries.com/v2/name/${c3}`),
+//     ]);
 
-    console.log(data.map(d => d[0].capital));
-  } catch (err) {
-    console.Error(err.message);
-  }
+//     console.log(data.map(d => d[0].capital));
+//   } catch (err) {
+//     console.Error(err.message);
+//   }
+// };
+
+// get3Countries('portugal', 'canada', 'tanzania');
+
+// Promise.race : means if one of all promises is settled no mater is fulfilled or rejected is win the race
+
+(async function () {
+  const res = await Promise.race([
+    getJSON(`https://restcountries.com/v2/name/italy`),
+    getJSON(`https://restcountries.com/v2/name/egypt`),
+    getJSON(`https://restcountries.com/v2/name/mexico`),
+  ]);
+
+  console.log(res[0]);
+})();
+
+const timeOut = function (sec) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error('Request took long time'));
+    }, sec * 1000);
+  });
 };
 
-get3Countries('portugal', 'canada', 'tanzania');
+Promise.race([
+  getJSON(`https://restcountries.com/v2/name/tanzania`),
+  timeOut(0.5),
+])
+  .then(res => console.log(res[0]))
+  .catch(err => console.error(err));
+
+// Promise.allSettled :  means return all promises is settled no mater is rejected or not
+Promise.allSettled([
+  Promise.resolve('Success'),
+  Promise.reject('Error'),
+  Promise.resolve('Anthor Success'),
+]).then(res => console.log(res));
+
+Promise.all([
+  Promise.resolve('Success'),
+  Promise.reject('Error'),
+  Promise.resolve('Anthor Success'),
+])
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
+
+// Promise.any :
+/*
+		will retuen the first fulfilled Promise 
+		and it will simply ignore rejected promises similar to race 
+		with the difference that rejected promises is ignore
+		*/
+// Promise.any([
+//   Promise.resolve('Success'),
+//   Promise.reject('Error'),
+//   Promise.resolve('Anthor Success'),
+// ])
+//   .then(res => console.log(res))
+//   .catch(err => console.error(err));
+
+Promise.any([
+  getJSON(`https://restcountries.com/v2/name/italy`),
+  getJSON(`https://restcountries.com/v2/name/egypt`),
+  getJSON(`https://restcountries.com/v2/name/mexico`),
+])
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
